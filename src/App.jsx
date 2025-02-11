9146336a1e15951f7baee9731ac99ceed44a3725
Pragathiswaran <mpragathiswaran@gmail.com>
2025-02-11 18:18:05 +0530

    fix : align center the No Task Added

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,16 @@ const App = () => {
     <>
       <div className="flex flex-col h-screen">
         <Navbar />
-          <div className="max-h-[calc(100vh-10rem)] overflow-y-auto mt-24 mb-36 md:mx-auto grid xl:grid-cols-3 
+          <div className="w-full max-h-[calc(100vh-10rem)] overflow-y-auto mt-24 mb-36 md:mx-auto grid xl:grid-cols-3 
             lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 justify-items-center lg:gap-8 md:gap-6 sm:gap-y-4 gap-y-4 
             scrollbar-hide scroll-smooth">
-              {todos.length > 0 ? <Card todoData={todos} updateTodoTask={updateTodo} removeTodoTask={removeTodo}/> : <h1 className='text-2xl font-semibold text-center'>No Todo's</h1>}  
+              {todos.length > 0 ? (
+                <Card todoData={todos} updateTodoTask={updateTodo} removeTodoTask={removeTodo}/>
+              ) : (
+                <div className="col-span-full flex flex-col items-center justify-center">
+                  <h1 className="text-2xl font-bold ">No Task Added</h1>
+                </div>
+              )}  
           </div>
         <Footer addTodo={addTodoHandler}/>
       </div>
